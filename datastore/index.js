0e5de4f5cd6d082e94741e9924018727e4a07f6e
Evan OShea <evanoshea21@gmail.com>
2022-11-15 14:54:56 -0800

    All Tests Passing up until Promises Mini-Sprint

--- datastore/index.js
+++ datastore/index.js
@@ -13,7 +13,7 @@ exports.create = (text, callback) => {//this CB is when CREATE is called
       callback(new Error('error'))
     } else {
       var filePath = exports.dataDir + `/${counterString}.txt`;
-      console.log('this is the path,   ', filePath);
+      // console.log('this is the path,   ', filePath);
       fs.writeFile(filePath , text, (err, data) => {
         if (err) {
           console.log(err);
@@ -39,42 +39,103 @@ exports.create = (text, callback) => {//this CB is when CREATE is called
   //   }
   // });
 };
-
+//readAll(err, todoList) -> todoList refers to [{id: id, text: text}, {id: id, text: text}...]
 exports.readAll = (callback) => {
-  var data = _.map(items, (text, id) => {
-    return { id, text };
-  });
-  callback(null, data);
+
+  // var data = _.map(items, (text, id) => {
+  //   return { id, text };
+  // });
+fs.readdir(exports.dataDir + '/', (err, filenames) => {
+  var arr = [];
+  if (err) {
+    // console.log('readDir error');
+    return;
+  }
+  for(var i = 0; i < filenames.length; i++) {
+    let obj = {id: filenames[i].slice(0,5)}
+    obj.text = fs.readFileSync(`${exports.dataDir}/${filenames[i]}`, 'utf8', (err, text) => {
+      if(err) {
+        throw('couldn\'t read file');
+      } else {
+        callback(null, filenames[i].slice(0,5));
+      }
+    });
+// console.log('OBJ ->,  ', obj);
+    arr.push(obj);
+  }
+  callback(null, arr);
+  // console.log('array' , arr)
+});
+ //counter --00003, for loop 0001
 };
 
 exports.readOne = (id, callback) => {
-  var text = items[id];
-  if (!text) {
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    callback(null, { id, text });
-  }
-};
+  var pathToFile = `${exports.dataDir}/${id}.txt`;
+  // console.log('ID IS', id, '\nPath to File,  ', pathToFile);
+  fs.readFile(pathToFile, 'utf8' , (err, text) => {
+    if (err) {
+      callback(new Error('error message'));
+    } else {
+      callback(null, { id, text });
+      // console.log('TEXT/ID', { id, text })
+    }
+  })
+  };
 
-exports.update = (id, text, callback) => {
-  var item = items[id];
-  if (!item) {
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    items[id] = text;
-    callback(null, { id, text });
-  }
-};
+  exports.update = (id, text, callback) => {
+    console.log('ID in Update ->  ', id);
+    var pathToFile = `${exports.dataDir}/${id}.txt`;
+    fs.readdir(`${exports.dataDir}/`, (err, filenames) => {
+      // if(err) {
+      //   callback(new Error('error in readdir'));
+      // } else {
+      console.log('FILENAMES ->', filenames);
+      if (filenames.includes(`${id}.txt`)) {
+        fs.writeFile(pathToFile, text, (err) => {
+          if (err) {
+            callback(new Error('error bruh'));
+          } else {
+            // callback(null, data);
+            var obj = {id, text};
+            console.log('OBJ in Update ->  ', obj);
+            callback(null, obj);
+          }
+        });
+      } else {
+       callback(new Error('file doesn\'t exist, so dont create new file. Good'));
+      }
+    });
+  };
+
+
+
+
+  // if (!item) {
+  //   callback(new Error(`No item with id: ${id}`));
+  // } else {
+  //   items[id] = text;
+  //   callback(null, { id, text });
+  // }
 
 exports.delete = (id, callback) => {
-  var item = items[id];
-  delete items[id];
-  if (!item) {
-    // report an error if item not found
-    callback(new Error(`No item with id: ${id}`));
+var pathToFile = `${exports.dataDir}/${id}.txt`;
+fs.unlink(pathToFile, (err) => {
+  if(err) {
+    callback(new Error('error in deleting file..'));
   } else {
-    callback();
+    callback(null);
   }
+});
+
+
+  // var item = items[id];
+  // delete items[id];
+  // if (!item) {
+  //   // report an error if item not found
+  //   callback(new Error(`No item with id: ${id}`));
+  // } else {
+  //   callback();
+  // }
 };
 
 // Config+Initialization code -- DO NOT MODIFY /////////////////////////////////
